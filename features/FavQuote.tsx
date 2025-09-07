2dd9c227699a9fc62f814caa0bba9530d4d379ef
Vaishali <aggarwal.vaishali785@gmail.com>
2025-09-07 20:38:58 +0530

    fav quote component for dashboard

---
+++ features/FavQuote.tsx
@@ -0,0 +1,21 @@
+import GlassCard from "../components/ui/GlassCard"
+type Props = {
+	quote: string
+	book: string
+}
+
+function FavQuote({ quote, book }: Props) {
+	return (
+		<GlassCard cardBg="p-6  px-8 bg-light-primary/44 max-w-[600] flex  flex-col justify-evenly">
+			{/* <div className="flex flex-col "> */}
+			<div className="text-light-secondary text-sm">Last Favorite Quote</div>
+			<blockquote className="text-light-secondary text-3xl font-bold">
+				{quote}
+			</blockquote>
+			<div className="text-light-secondary text-sm">-{book}</div>
+			{/* </div> */}
+		</GlassCard>
+	)
+}
+
+export default FavQuote

d55bd4e444967a7e06c2362ccaa965c0255163b1
Vaishali <dev.vaishali785@gmail.com>
2025-09-08 20:54:52 +0530

    moved all the folders inside src but outside app (to avoid routing

---
+++ src/features/KPIs.tsx
@@ -0,0 +1,30 @@
+import KPICard from "../components/custom-ui/KPICard"
+
+function KPIs() {
+	// tab: one line - 640 -1024
+	//
+	return (
+		// <div className="flex gap-4 max-md:flex-wrap">
+		// 	<div className="flex gap-4  flex-col ">
+		// 		<KPICard cardCount="4" cardTitle={"Reading Streak"} />
+		// 		<KPICard cardCount="345" cardTitle={"Total Pages Read"} />
+		// 	</div>
+		// 	<div className="flex gap-4 flex-col ">
+		// 		<KPICard cardCount="1" cardTitle={"Books Read This Month"} />
+		// 		<KPICard cardCount="10 / 12" cardTitle={"Annual Goal"} />
+		// 	</div>
+		// </div>
+		<div className="grid grid-cols-2 gap-4 md:max-lg:grid-cols-4">
+			{/* <div className="flex gap-4  flex-col "> */}
+			<KPICard cardCount="4" cardTitle={"Reading Streak"} />
+			<KPICard cardCount="1" cardTitle={"Books Read This Month"} />
+			{/* </div>
+			<div className="flex gap-4 flex-col "> */}
+			<KPICard cardCount="345" cardTitle={"Total Pages Read"} />
+			<KPICard cardCount="10 / 12" cardTitle={"Annual Goal"} />
+			{/* </div> */}
+		</div>
+	)
+}
+
+export default KPIs

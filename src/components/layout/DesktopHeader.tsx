a63d5abe94f71c91c393b39999dd745834d6ab4d
Vaishali <dev.vaishali785@gmail.com>
2025-09-15 20:54:41 +0530

    added button to add new book in header

--- src/components/layout/DesktopHeader.tsx
+++ src/components/layout/DesktopHeader.tsx
@@ -1,7 +1,9 @@
+import Button from "../custom-ui/ModelWithButton"
 import GlassCard from "../custom-ui/GlassCard"
 import SearchBar from "../custom-ui/SearchBar"
 import ThemeToggle from "../custom-ui/ThemeToggle"
 import UserAvatar from "../custom-ui/UserAvatar"
+import ModelWithButton from "../custom-ui/ModelWithButton"
 
 function DesktopHeader() {
 	return (
@@ -11,6 +13,13 @@ function DesktopHeader() {
 				<div className="text-light-primary font-bold text-2xl">Hi Vaishali</div>
 				<div className="flex gap-6">
 					<SearchBar />
+					<ModelWithButton
+						title="Add Book"
+						icon="Plus"
+						size={12}
+						modelTitle="Add New Book"
+						modelDesc="Enter details below to add a new book to your collection."
+					></ModelWithButton>
 					<div className="flex gap-6 justify-end">
 						<ThemeToggle />
 						<div>

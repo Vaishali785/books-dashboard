d55af7a66a81378c6a53b5821960cf8fb545974a
Vaishali <dev.vaishali785@gmail.com>
2025-09-22 23:00:57 +0530

    created dummy data for line chart

---
+++ src/lib/data.ts
@@ -0,0 +1,56 @@
+export const lineData = [
+    {
+        "id": "japan",
+        "data": [
+            {
+                "x": "plane",
+                "y": 243
+            },
+            {
+                "x": "helicopter",
+                "y": 66
+            },
+            {
+                "x": "boat",
+                "y": 144
+            },
+            {
+                "x": "train",
+                "y": 232
+            },
+            {
+                "x": "subway",
+                "y": 84
+            },
+            {
+                "x": "bus",
+                "y": 279
+            },
+            {
+                "x": "car",
+                "y": 209
+            },
+            {
+                "x": "moto",
+                "y": 153
+            },
+            {
+                "x": "bicycle",
+                "y": 220
+            },
+            {
+                "x": "horse",
+                "y": 255
+            },
+            {
+                "x": "skateboard",
+                "y": 257
+            },
+            {
+                "x": "others",
+                "y": 14
+            }
+        ]
+    },
+
+]

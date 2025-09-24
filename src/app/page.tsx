2c1b1eb59e73e2e0f428122ded1fa60e4a9b23bc
Vaishali <dev.vaishali785@gmail.com>
2025-09-24 21:28:43 +0530

    added pie chart and bar chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import GlassCard from "../../components/ui/GlassCard"
-import ThemeBgImage from "../../components/ui/ThemeBgImage"
-import DesktopHeader from "../../components/layout/DesktopHeader"
-import KPIs from "../../features/KPIs"
-import FavQuote from "../../features/FavQuote"
-import DesktopSidebar from "../../components/layout/DesktopSidebar"
+import KPIs from "../features/KPIs"
+import FavQuote from "../features/FavQuote"
+import ThemeBgImage from "@/components/custom-ui/ThemeBgImage"
+import GlassCard from "@/components/custom-ui/GlassCard"
+import DesktopSidebar from "@/components/layout/DesktopSidebar"
+import DesktopHeader from "@/components/layout/DesktopHeader"
+import LineChart from "@/components/custom-ui/charts/LineChart"
+import { barData, genreData, lineData, pagesReadData } from "@/lib/data"
+import BarChart from "@/components/custom-ui/charts/BarChart"
+import PieChart from "@/components/custom-ui/charts/PieChart"
 
 export default function Home() {
 	return (
@@ -19,21 +23,24 @@ export default function Home() {
 					<div className="flex gap-6 justify-end sm:max-lg:flex-wrap sm:max-lg:flex-col sm:max-lg:w-full">
 						<KPIs />
 						<FavQuote
-							quote="Experience is what you get when you didn’t get what you wanted."
-							book="The Last Lecture"
+							// quote="Experience is what you get when you didn’t get what you wanted."
+							// book="The Last Lecture"
+							quote="Viva La Vida"
+							book="Twinkling Watermelon"
 						/>
 					</div>
 					<div className="flex md:justify-between  mt-6 sm:max-lg:flex-wrap ">
 						<div className="flex flex-col max-md:w-full md:w-[48.5%] w-[490px] gap-4">
-							<GlassCard classes="w-full h-[200px]">
-								<h1 className="p-4">Hello World</h1>
+							<GlassCard classes="w-full h-[200px] p-0">
+								<LineChart data={lineData} />
+								{/* <BarChart data={pagesReadData} /> */}
 							</GlassCard>
 							<GlassCard classes="h-[200px] w-full">
 								<h1 className="p-4">Hello World</h1>
 							</GlassCard>
 						</div>
 						<GlassCard classes="max-md:w-full md:w-[48.5%] w-[490px] h-[415px]">
-							<h1 className="p-4">Hello World</h1>
+							<PieChart data={genreData} />
 						</GlassCard>
 					</div>
 				</div>

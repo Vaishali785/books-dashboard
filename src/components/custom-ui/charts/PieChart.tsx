bfddd34cc27eb1748c9c40b5e8a9729e917a35f1
Vaishali <dev.vaishali785@gmail.com>
2025-10-16 20:30:45 +0530

    pie chart styling changes

--- src/components/custom-ui/charts/PieChart.tsx
+++ src/components/custom-ui/charts/PieChart.tsx
@@ -4,30 +4,30 @@ import { ResponsivePie } from "@nivo/pie"
 const PieChart = ({ data /* see data tab */ }) => (
 	<ResponsivePie /* or Pie for fixed dimensions */
 		data={data}
-		margin={{ top: 10, right: 80, bottom: 80, left: 80 }}
+		margin={{ top: 10, right: 80, bottom: 20, left: 80 }}
 		innerRadius={0.5}
 		padAngle={0.6}
 		cornerRadius={2}
-		enableArcLabels={false}
+		enableArcLabels={true}
 		activeOuterRadiusOffset={8}
 		arcLinkLabel={(d) => `${d.id} (${d.value})`}
-		arcLinkLabelsSkipAngle={10}
-		arcLinkLabelsTextColor="#333"
+		// arcLinkLabelsSkipAngle={10}
+		arcLinkLabelsTextColor="white"
 		arcLinkLabelsThickness={2}
 		arcLinkLabelsColor={{ from: "color" }}
 		arcLabelsSkipAngle={10}
-		// arcLabelsTextColor={{ from: "color", modifiers: [["brighter", 3]] }}
+		arcLabelsTextColor={{ from: "color", modifiers: [["brighter", 3]] }}
 		colors={{ scheme: "purpleRed_green" }}
-		legends={[
-			{
-				anchor: "bottom",
-				direction: "row",
-				translateY: 56,
-				itemWidth: 100,
-				itemHeight: 18,
-				symbolShape: "circle",
-			},
-		]}
+		// legends={[
+		// 	{
+		// 		anchor: "bottom",
+		// 		direction: "row",
+		// 		translateY: 56,
+		// 		itemWidth: 100,
+		// 		itemHeight: 18,
+		// 		symbolShape: "circle",
+		// 	},
+		// ]}
 		theme={{
 			tooltip: {
 				container: {

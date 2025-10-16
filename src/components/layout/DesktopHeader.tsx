f39e01911deba62436f661eb35bf75b7775a7fb6
Vaishali <dev.vaishali785@gmail.com>
2025-10-16 20:05:36 +0530

    sticky header

--- src/components/layout/DesktopHeader.tsx
+++ src/components/layout/DesktopHeader.tsx
@@ -9,8 +9,10 @@ function DesktopHeader() {
 	return (
 		<div className="flex gap-6 w-full">
 			{/* <div className="w-2/7 h-[30px]"></div> */}
-			<div className="z-10 relative flex gap-6 mb-6 px-4 justify-between w-full">
-				<div className="text-light-primary font-bold text-2xl">Hi Vaishali</div>
+			<div className="z-10 relative flex gap-6 mb-6 px-4 justify-between items-center w-full">
+				<div className="text-white lg:text-light-primary font-bold text-2xl">
+					Hi Vaishali
+				</div>
 				<div className="flex gap-6">
 					<SearchBar />
 					<ModelWithButton

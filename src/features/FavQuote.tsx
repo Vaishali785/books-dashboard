2bf0fddb84e3b74254374e7f857afd469acc5991
Vaishali <dev.vaishali785@gmail.com>
2025-10-30 14:48:43 +0530

    added dynamic function calls for fav quote

--- src/features/FavQuote.tsx
+++ src/features/FavQuote.tsx
@@ -1,18 +1,20 @@
+"use client"
+import { getRecentFavQuote } from "@/lib/utils"
 import GlassCard from "../components/custom-ui/GlassCard"
-type Props = {
-	quote: string
-	book: string
-}
 
-function FavQuote({ quote, book }: Props) {
+function FavQuote() {
+	const bookWithQuote = getRecentFavQuote()
+	const favQuote = bookWithQuote?.favQuote || "Viva La Vida"
+	const bookName = bookWithQuote?.name || "Twinkling Watermelon"
+
 	return (
 		<GlassCard cardBg="p-6 px-8 bg-light-primary/44 max-sm:max-w-[600] flex  flex-col justify-evenly sm:max-lg:w-full lg:flex-1">
 			{/* <div className="flex flex-col "> */}
 			<div className="text-light-secondary text-sm">Last Favorite Quote</div>
 			<blockquote className="text-light-secondary text-3xl font-bold">
-				{quote}
+				{favQuote}
 			</blockquote>
-			<div className="text-light-secondary text-sm">-{book}</div>
+			<div className="text-light-secondary text-sm">-{bookName}</div>
 			{/* </div> */}
 		</GlassCard>
 	)

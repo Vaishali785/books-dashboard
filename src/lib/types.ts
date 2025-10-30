9b5e3b4bb86d5b75aea3cc6353b3edea7d8484e3
Vaishali <dev.vaishali785@gmail.com>
2025-10-30 14:46:13 +0530

    ts types file

---
+++ src/lib/types.ts
@@ -0,0 +1,11 @@
+
+export type BookData = {
+    "name": string,
+    "author": string,
+    "genre": string,
+    "status": string,
+    "favQuote": string,
+    "startDate": Date,
+    "endDate": Date
+}
+// export type BookData = Array<BookUnitData>

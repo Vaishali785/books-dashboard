3e5dd0f36ad2dc83d22f12463c0e86047b92e94b
Vaishali <dev.vaishali785@gmail.com>
2025-11-02 20:35:50 +0530

    new component for fav quote page

--- src/features/FavQuote.tsx
+++ src/features/FavQuote.tsx
@@ -1,20 +1,42 @@
 "use client"
-import { getRecentFavQuote } from "@/lib/utils"
+import clsx from "clsx"
+import { QuoteIcon } from "lucide-react"
 import GlassCard from "../components/custom-ui/GlassCard"
 
-function FavQuote() {
-	const bookWithQuote = getRecentFavQuote()
-	const favQuote = bookWithQuote?.favQuote || "Viva La Vida"
-	const bookName = bookWithQuote?.name || "Twinkling Watermelon"
+function FavQuote({
+	quote,
+	bookTitle,
+	customClasses,
+	hideFavQuoteHeading,
+	showQuotesIcon,
+}: {
+	quote?: string
+	bookTitle?: string
+	customClasses?: string
+	hideFavQuoteHeading?: boolean
+	showQuotesIcon?: boolean
+}) {
+	const favQuote = quote || "Viva La Vida"
+	const bookName = bookTitle || "Twinkling Watermelon"
 
 	return (
-		<GlassCard cardBg="p-6 px-8 bg-light-primary/44 max-sm:max-w-[600] flex  flex-col justify-evenly sm:max-lg:w-full lg:flex-1">
-			{/* <div className="flex flex-col "> */}
-			<div className="text-light-secondary text-sm">Last Favorite Quote</div>
-			<blockquote className="text-light-secondary text-3xl font-bold">
-				{favQuote}
-			</blockquote>
-			<div className="text-light-secondary text-sm">-{bookName}</div>
+		<GlassCard
+			cardBg={clsx(
+				"p-6 px-8 bg-light-primary/44 max-sm:max-w-[600] flex  flex-col items-center justify-center sm:max-lg:w-full lg:flex-1",
+				customClasses
+			)}
+		>
+			{showQuotesIcon && (
+				<span>
+					<QuoteIcon />
+				</span>
+			)}
+			<div className="flex justify-center flex-col h-full text-center gap-2">
+				<blockquote className="text-light-secondary text-3xl font-bold">
+					{favQuote}
+				</blockquote>
+				<div className="text-light-secondary text-sm">-{bookName}</div>
+			</div>
 			{/* </div> */}
 		</GlassCard>
 	)
